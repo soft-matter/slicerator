92a3094b2ba8640b8da74e20b29988821f01b37b
danielballan <daniel.b.allan@gmail.com>
2015-07-30 15:07:28 -0400

    BLD: typo in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='history',
       version='0.1.0',
-      author='Daniel B. Allan'
+      author='Daniel B. Allan',
       py_modules=['slicerator'],
       description='A lazy-loading, fancy-sliceable iterable.',
       url='http://github.com/soft-matter/slicerator',
