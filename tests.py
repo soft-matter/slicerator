bbebe8d2917c19ed06e5deddd494e6a37912b1bd
caspervdw <caspervdw@gmail.com>
2015-11-23 15:51:01 +0100

    CLN Remove prints in test

--- tests.py
+++ tests.py
@@ -122,13 +122,9 @@ def test_slice_of_slice_of_slice_of_slice():
     compare_slice_to_list(slice2, list('cegi'))
     slice3 = slice2[:]
     compare_slice_to_list(slice3, list('cegi'))
-    print('define slice4')
     slice4 = slice3[:-1]
-    print('compare slice4')
     compare_slice_to_list(slice4, list('ceg'))
-    print('define slice4a')
     slice4a = slice3[::-1]
-    print('compare slice4a')
     compare_slice_to_list(slice4a, list('igec'))
 
 
